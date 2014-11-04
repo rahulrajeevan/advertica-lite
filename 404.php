7309b85dd33f7a70264464c72b74fad09b4315e3
rahulc-nrift <rahulc@nrifintech.com>
2014-11-04 10:00:19 +0530

    FXB-1839

--- 404.php
+++ 404.php
@@ -16,7 +16,7 @@ get_header(); ?>
 						<div class="error-txt-first"><?php _e( '404', 'advertica-lite' ); ?></div>
 						<p><?php _e( 'Sorry, but the requested resource was not found on this site. Please try again or contact the administrator for assistance.', 'advertica-lite' ); ?></p>
 					</div>
-					<!-- post --> 
+
 				</div>
 				<!-- post -->
 			</div>
