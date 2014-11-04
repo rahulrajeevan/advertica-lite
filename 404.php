3f93bc0c027126a467af0403bc6b9c749d45db39
rahulc-nrift <rahulc@nrifintech.com>
2014-11-04 10:12:03 +0530

    test Second Commit

--- 404.php
+++ 404.php
@@ -6,9 +6,7 @@
 
 get_header(); ?>
 			<p><?php _e( 'Sorry, but the requested resource was not found on this site. Please try again or contact the administrator for assistance.', 'advertica-lite' ); ?></p>
-					</div>
-
-				</div>
+				div>
 				<!-- post -->
 			</div>
 			<!-- content --> 
