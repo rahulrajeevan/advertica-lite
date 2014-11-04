6d20aef9a9405160bbc0ee140358b06bc7d44c95
rahulc-nrift <rahulc@nrifintech.com>
2014-11-04 10:07:09 +0530

    third commit

--- 404.php
+++ 404.php
@@ -5,11 +5,7 @@
  */
 
 get_header(); ?>
-<?php global $advertica_shortname; ?>
-
-<div class="
-						<div class="error-txt-first"><?php _e( '404', 'advertica-lite' ); ?></div>
-						<p><?php _e( 'Sorry, but the requested resource was not found on this site. Please try again or contact the administrator for assistance.', 'advertica-lite' ); ?></p>
+			<p><?php _e( 'Sorry, but the requested resource was not found on this site. Please try again or contact the administrator for assistance.', 'advertica-lite' ); ?></p>
 					</div>
 
 				</div>
