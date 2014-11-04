d2a39ec60bd7a7680ac71440571377802d4c4ae3
rahulc-nrift <rahulc@nrifintech.com>
2014-11-04 09:58:19 +0530

    FXB-1832

--- comments.php
+++ comments.php
@@ -1,13 +1,5 @@
 <?php
-/**
- * The template for displaying Comments.
- * The area of the page that contains comments and the comment form.
- */
 
-/*
- * If the current post is protected by a password and the visitor has not yet
- * entered the password we will return early without loading the comments.
- */
 ?>
 
 <?php
